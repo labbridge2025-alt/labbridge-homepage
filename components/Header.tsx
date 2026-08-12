dfec3bf01d8a1598dd54d7c27ea4d9d6a938bc0e
barcode <jungeunhee0625@gmail.com>
2026-08-12 09:43:36 +0900

    메인 구성 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -57,10 +57,10 @@ export default function Header() {
         </a>
 
         <nav className="hidden items-center gap-8 text-lg font-bold lg:flex">
+          <a href="/process">회사소개</a>
           <a href="/products">제형보러가기</a>
           <a href="/containers">용기보러가기</a>
 <a href="/packages">패키지보러가기</a>
-          <a href="/process">회사소개</a>
           <a href="/portfolio">포트폴리오</a>
           <a href="/estimate">문의하기</a>
 
